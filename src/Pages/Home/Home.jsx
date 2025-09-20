21e4f7374323f3b063b45bc98d92a2c2103641f6
dineshkumar24v <dineshkumar24v@gmail.com>
2025-09-20 14:03:32 +0530

    commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Login from '../Login/Login'
 import SignUp from '../SignUp/SignUp'
 import './Home.css'
+import { useState } from 'react'
 const Home = () => {
+   const [focusLoginEmail, setFocusLoginEmail] = useState(false);
+
+   const handleSignupSuccess = () => {
+    setFocusLoginEmail(true);
+    setTimeout(() => setFocusLoginEmail(false), 500); // Reset after focus
+  };
+
   return (
     <div className='homeContainer'>
-      <Login/>
+      <Login focusEmail={focusLoginEmail} />
       <div className='seperator'></div>
-      <SignUp/>
+      <SignUp onSignupSuccess={handleSignupSuccess} />
     </div>
   )
 }
